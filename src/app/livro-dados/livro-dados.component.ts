8e873b01516530acb1b2ced42bbc9830129353c2
Clayton Meireles <clayton.projectpython@gmail.com>
2025-11-23 11:16:02 -0300

    inicial commit

---
+++ src/app/livro-dados/livro-dados.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+
+@Component({
+  selector: 'app-livro-dados',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './livro-dados.component.html',
+  styleUrl: './livro-dados.component.css'
+})
+export class LivroDadosComponent implements OnInit {
+  public livro: Livro;
+  public autoresForm: string = '';
+  public editoras: Array<Editora> = [];
+
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router
+  ) {
+    this.livro = new Livro();
+    this.livro.autores = [];
+  }
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+
+  incluir = (): void => {
+    this.livro.autores = this.autoresForm.split('\n').map(autor => autor.trim()).filter(autor => autor.length > 0);
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl('/lista');
+  }
+}
